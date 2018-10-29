f8e029d4ae18ce969560625236d9ac9f05c48b80
MarcoGalvao01 <44274096+MarcoGalvao01@users.noreply.github.com>
2018-10-29 17:49:01 -0300

    Add files via upload

---
+++ src/fatec/poo/model/Matricula.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fatec.poo.model;
+
+/**
+ *
+ * @author Aluno - Gti-Ads
+ */
+public class Matricula {
+
+    private String Data;
+    private int QtdeFaltas;
+    private double Nota;
+    
+}
---
+++ src/fatec/poo/model/Pessoa.java
@@ -0,0 +1,162 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fatec.poo.model;
+
+/**
+ *
+ * @author Aluno - Gti-Ads
+ */
+public abstract class Pessoa 
+{
+    private String Nome;
+    private String DataNasc;
+    private String Endereco;
+    private int Numero;
+    private String Bairro;
+    private String Cidade;
+    private String Estado;
+    private String CEP;
+    private String Telefone;
+    private String Celular;
+    private String Sexo;
+    private String EstadoCivil;
+    private String RG;
+    private String CPF;
+    private String Email;
+
+    
+    public Pessoa(String Nome, String CPF) {
+        this.Nome = Nome;
+        this.CPF = CPF;
+    }
+
+    
+    
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public String getDataNasc() {
+        return DataNasc;
+    }
+
+    public void setDataNasc(String DataNasc) {
+        this.DataNasc = DataNasc;
+    }
+
+    public String getEndereco() {
+        return Endereco;
+    }
+
+    public void setEndereco(String Endereco) {
+        this.Endereco = Endereco;
+    }
+
+    public int getNumero() {
+        return Numero;
+    }
+
+    public void setNumero(int Numero) {
+        this.Numero = Numero;
+    }
+
+    public String getBairro() {
+        return Bairro;
+    }
+
+    public void setBairro(String Bairro) {
+        this.Bairro = Bairro;
+    }
+
+    public String getCidade() {
+        return Cidade;
+    }
+
+    public void setCidade(String Cidade) {
+        this.Cidade = Cidade;
+    }
+
+    public String getEstado() {
+        return Estado;
+    }
+
+    public void setEstado(String Estado) {
+        this.Estado = Estado;
+    }
+
+    public String getCEP() {
+        return CEP;
+    }
+
+    public void setCEP(String CEP) {
+        this.CEP = CEP;
+    }
+
+    public String getTelefone() {
+        return Telefone;
+    }
+
+    public void setTelefone(String Telefone) {
+        this.Telefone = Telefone;
+    }
+
+    public String getCelular() {
+        return Celular;
+    }
+
+    public void setCelular(String Celular) {
+        this.Celular = Celular;
+    }
+
+    public String getSexo() {
+        return Sexo;
+    }
+
+    public void setSexo(String Sexo) {
+        this.Sexo = Sexo;
+    }
+
+    public String getEstadoCivil() {
+        return EstadoCivil;
+    }
+
+    public void setEstadoCivil(String EstadoCivil) {
+        this.EstadoCivil = EstadoCivil;
+    }
+
+    public String getRG() {
+        return RG;
+    }
+
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+    
+    
+    
+    
+    
+}
